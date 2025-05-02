8ee6fea466745bed144f7082909343dced565ead
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-05-02 02:05:45 +0200

    Undo automatic PNG unpacking; KRA file itself can now be used as texture!

--- Assets/Scripts/Editor/KritaImporter.cs
+++ Assets/Scripts/Editor/KritaImporter.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.IO;
 using System.IO.Compression;
 using UnityEditor.AssetImporters;
@@ -5,7 +6,7 @@ using UnityEngine;
 
 namespace Editor
 {
-	//inspired by https://github.com/justalexi/unity_krita_exporter
+	// Inspired by https://github.com/justalexi/unity_krita_exporter
 
 	[ScriptedImporter(1, "kra")]
 	public class KritaImporter : ScriptedImporter
@@ -13,35 +14,49 @@ namespace Editor
 		// A .kra file contains a file named "mergedimage.png"
 		// which contains the rendered image as you see it on your canvas
 		// (see https://docs.krita.org/en/general_concepts/file_formats/file_kra.html).
-		private const string PNG_FILE_NAME_INSIDE_KRA_ARCHIVE = "mergedimage.png";
+		public const string PNG_FILE_NAME_INSIDE_KRA_ARCHIVE = "mergedimage.png";
+
+		public static ZipArchiveEntry GetPNG(ZipArchive zip, string assetPath)
+		{
+			// Get the asset .png file inside
+			ZipArchiveEntry? png = zip.GetEntry(PNG_FILE_NAME_INSIDE_KRA_ARCHIVE);
+			if (png == null)
+			{
+				throw new FileNotFoundException($"No {PNG_FILE_NAME_INSIDE_KRA_ARCHIVE} file found in {assetPath}");
+			}
+
+			return png;
+		}
 
 		public override void OnImportAsset(AssetImportContext ctx)
 		{
-			//Get directory of the .kra file
-			FileInfo file = new(ctx.assetPath);
-			DirectoryInfo directory = file.Directory;
-			if (directory == null)
-			{
-				Debug.LogError($"Could not get directory of {ctx.assetPath}");
-				return;
-			}
+			// Open the .kra file as a zip
+			using ZipArchive zip = ZipFile.OpenRead(assetPath);
+			ZipArchiveEntry png = GetPNG(zip, ctx.assetPath);
 
-			//Open the .kra file as a zip and get the PNG file inside
-			ZipArchive zip = ZipFile.OpenRead(ctx.assetPath);
-			ZipArchiveEntry png = zip.GetEntry(PNG_FILE_NAME_INSIDE_KRA_ARCHIVE);
-			if (png == null)
+			// Load the .png file into a Texture2D
+			Texture2D texture = new(0, 0);
+			using (Stream stream = png.Open())
 			{
-				Debug.LogError($"No {PNG_FILE_NAME_INSIDE_KRA_ARCHIVE} file found in {ctx.assetPath}");
-				return;
+				byte[] bytes = new byte[png.Length];
+				int result = stream.Read(bytes, 0, bytes.Length);
+				if (result == 0)
+				{
+					Debug.LogError($"Could not read {png.Length} bytes from {png.FullName}");
+					return;
+				}
+				if (result != png.Length)
+				{
+					Debug.LogError($"Could not read all {png.Length} bytes from {png.FullName}");
+					return;
+				}
+				texture.LoadImage(bytes);
 			}
+			texture.Apply();
 
-			//Unpack the PNG file
-			string pngName = Path.GetFileNameWithoutExtension(ctx.assetPath) + ".png";
-			using Stream stream = png.Open();
-			string pngPath = Path.Combine(directory.FullName, pngName);
-			Debug.Log($"Unpacking {ctx.assetPath}/{PNG_FILE_NAME_INSIDE_KRA_ARCHIVE} to {pngPath}");
-			using FileStream fileStream = new(pngPath, FileMode.Create);
-			stream.CopyTo(fileStream);
+			// Save the texture, so it can be used in the editor
+			ctx.AddObjectToAsset("texture", texture);
+			ctx.SetMainObject(texture);
 		}
 	}
 }
